216b127aef510be4a21bb7d5fe63f99229596052
Matthias Riegler <me@xvzf.tech>
2020-02-29 12:24:21 +0100

    started working on a new setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ex
+
+CONFIGS="zsh"
+
+BREW_PACKAGES="fzf nvim ripgrep exa zsh git go tmux stow"
+BREW_CASK_PACKAGES="docker google-chrome visual-studio-code"
+
+# Install packages
+case `uname` in
+  # MAC specific commands
+  Darwin)
+    brew install $BREW_PACKAGES
+    brew cask install $BREW_CASK_PACKAGES
+    ;;
+  Linux)
+    ;;
+esac
+
+# Symlink build files
+for CONFIG in $CONFIGS; do
+	stow $CONFIG
+done
