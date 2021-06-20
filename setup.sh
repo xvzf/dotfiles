c3e0ad73b3cc250e71f8c99bad6ea7a28cca5510
Matthias Riegler <matthias.riegler@taotesting.com>
2021-06-20 14:44:20 +0200

    update
    Signed-off-by: Matthias Riegler <matthias.riegler@taotesting.com>

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh -ex
 
 ### Managed configs
-CONFIGS="zsh git tmux vim nvim fzf"
-
-### Neoclide Plugins
-COC_PLUGINS="coc-go"
+CONFIGS=""
 
 ### MacOS Packages
-BREW_PACKAGES="fzf neovim jq ripgrep bat exa zsh git go tmux stow python3 node k3d mas php composer packer terraform starship stern"
-BREW_CASK_PACKAGES="docker google-chrome visual-studio-code slack jira-client google-cloud-sdk drawio kitty"
+BREW_PACKAGES="fzf neovim jq yq ripgrep bat exa zsh git go tmux python3 node k3d mas php composer packer terraform starship stern aquasecurity/trivy/trivy"
+BREW_CASK_PACKAGES="docker google-chrome visual-studio-code slack google-cloud-sdk drawio"
 
 
 # ======================
@@ -20,19 +17,6 @@ case `uname` in
   # MAC specific commands
   Darwin)
     brew install $BREW_PACKAGES
-    brew cask install $BREW_CASK_PACKAGES
+    brew install --cask $BREW_CASK_PACKAGES
     ;;
 esac
-
-# Symlink build files
-for CONFIG in $CONFIGS; do
-	echo "[+] Copying config files for ${CONFIG} ..."
-  stow --dir=$(pwd) --target=$HOME $CONFIG
-done
-
-# Auto install nvim packages & coc extensions
-nvim --headless -c ":call dein#install() | :call dein#check_install() | q"
-for COC_PLUGIN in $COC_PLUGINS; do
-  nvim --headless -c ":CocInstall ${COC_PLUGIN}" -c "q"
-done
-nvim -S --headless -c ":GoInstallBinaries" -c "q" || true
--- zsh/.zsh/plugins/kubectl_lazyload.sh
+++
@@ -1,7 +0,0 @@
-function kubectl() {
-    if ! type __start_kubectl >/dev/null 2>&1; then
-        source <(command kubectl completion zsh)
-    fi
-
-    command kubectl "$@"
-}
